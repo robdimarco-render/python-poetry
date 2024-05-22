fbacd3822ec8b25cef5298eedbd134aaa3672ebe
Rob Di Marco <robdimarco@render.com>
2024-05-22 15:29:19 -0400

    Fix scripts

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 installed_python_version=$(python -c 'import platform; print(platform.python_version())')
 
-if [[ "$installed_python_version" != "3.11.8" ]]; 
+if [[ "$installed_python_version" != "3.11.8" ]]; then
   echo "Have version $installed_python_version"
   exit 1
 fi
